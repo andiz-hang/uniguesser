d1bce736526883782aa4161683d43cceaa01745c
jla293 <jacob.lee@sfu.ca>
2021-06-26 10:21:23 -0700

    Add database initilization script and express project template

---
+++ setup/database.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS university;
+DROP TABLE IF EXISTS campus;
+DROP TABLE IF EXISTS game_session;
+
+CREATE TABLE user (
+    user_id SERIAL PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    phone TEXT,
+    email TEXT,
+);
+
+CREATE TABLE university (
+    university_id SERIAL PRIMARY KEY NOT NULL,
+    university_name TEXT NOT NULL
+);
+
+CREATE TABLE campus (
+    campus_id SERIAL PRIMARY KEY NOT NULL,
+    university_id INTEGER NOT NULL,
+    street TEXT NOT NULL,
+    city TEXT NOT NULL,
+    province TEXT,
+    country TEXT NOT NULL,
+    postal_code TEXT NOT NULL,
+    FOREIGN KEY (university_id) REFERENCES university(university_id),
+);
+
+CREATE TABLE game_session (
+    session_id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER NOT NULL,
+    score NUMERIC,
+    created_at TIMESTAMPTZ NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+);
