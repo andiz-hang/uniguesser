a8ee553e7d9e5d729409a8b00d8d2a3b55b584fb
jla293 <jacob.lee@sfu.ca>
2021-07-06 23:17:23 -0700

    Add test data to database

--- setup/database.sql
+++ setup/database.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS "user" CASCADE;
 DROP TABLE IF EXISTS university CASCADE;
 DROP TABLE IF EXISTS campus CASCADE;
 DROP TABLE IF EXISTS game_session CASCADE;
@@ -13,7 +13,8 @@ CREATE TABLE "user" (
 
 CREATE TABLE university (
     university_id SERIAL PRIMARY KEY NOT NULL,
-    university_name TEXT NOT NULL
+    university_name TEXT NOT NULL,
+    abbreviation TEXT
 );
 
 CREATE TABLE campus (
@@ -36,3 +37,17 @@ CREATE TABLE game_session (
     created_at TIMESTAMPTZ NOT NULL,
     FOREIGN KEY (user_id) REFERENCES "user"(user_id)
 );
+
+insert into university (university_name, abbreviation) 
+			values ('Simon Fraser University', 'SFU');
+insert into university (university_name, abbreviation) 
+			values ('University of British Columbia', 'UBC');
+
+insert into campus (university_id, campus_name, street, city, province, country, postal_code) 
+			values (1, 'SFU Burnaby', '8888 University Dr', 'Burnaby', 'BC', 'Canada', 'V5A1S6');
+insert into campus (university_id, campus_name, street, city, province, country, postal_code) 
+			values (1, 'SFU Surrey', '13450 102 Ave #250', 'Surrey', 'BC', 'Canada', 'V3T0A3');
+insert into campus (university_id, campus_name, street, city, province, country, postal_code) 
+			values (2, 'UBC Vancouver', '2329 West Mall', 'Vancouver', 'BC', 'Canada', 'V6T1Z4');
+insert into campus (university_id, campus_name, street, city, province, country, postal_code) 
+			values (2, 'UBC Okanagan', '3333 University Way', 'Kelowna', 'BC', 'Canada', 'V1V1V7');                        
