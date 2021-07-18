61d7f5f757162b11d8c7ca4804a8b24ec9c04474
jtw14 <jtw14@sfu.ca>
2021-07-17 22:25:31 -0400

    Utilize start game button
    Attaches startGame() function with start-button. Makes sure only the first attempt is valid to prevent multiple game sessions

--- src/public/javascripts/game.js
+++ src/public/javascripts/game.js
@@ -2,73 +2,84 @@ var schoolList = [];
 var schoolIndex = 0;
 var gameTimer;
 var score = 0;
+var firstAttempt = true;
 
-$(document).ready(function(){
-    startGame();
+$(document).ready(function () {
+  $("#start-button").on("click", () => {
+    if (firstAttempt) {
+      firstAttempt = false;
+      startGame();
+    }
+  });
 
-    $("#end-button").on("click", () => {
-        endGame();
-    })
+  $("#end-button").on("click", () => {
+    endGame();
+  });
 
-    $("#confirm").on("click", () => {
-        let selectedOption = $("#schools")[0].selectedOptions[0].innerHTML
-        if (selectedOption == schoolList[schoolIndex].university_name) {
-            score++;
-            $("#score")[0].innerHTML = score;
-        }
-        if (schoolIndex < schoolList.length-1) {
-            nextSchool();
-        }
-        else {
-            endGame();
-        }
-    });
+  $("#confirm").on("click", () => {
+    let selectedOption = $("#schools")[0].selectedOptions[0].innerHTML;
+    if (selectedOption == schoolList[schoolIndex].university_name) {
+      score++;
+      $("#score")[0].innerHTML = score;
+    }
+    if (schoolIndex < schoolList.length - 1) {
+      nextSchool();
+    } else {
+      endGame();
+    }
+  });
 });
 
-
 function startGame() {
-    $.ajax({
-        method: 'get',
-        url: '/universities',
-        success: data => {
-            schoolList = data;
-            $("#location_counter").text(`Location: ${schoolIndex+1}/${schoolList.length}`);
-            displayPhotoByUrl(schoolList[0].photo_url)
+  $.ajax({
+    method: "get",
+    url: "/universities",
+    success: (data) => {
+      schoolList = data;
+      $("#location_counter").text(
+        `Location: ${schoolIndex + 1}/${schoolList.length}`
+      );
+      displayPhotoByUrl(schoolList[0].photo_url);
 
-            var startTime = new Date();
+      var startTime = new Date();
 
-            gameTimer = setInterval(() => {
-                currentTime = new Date();
-                timeDiff = Math.floor((currentTime - startTime) / 1000);
-               $("#timer")[0].innerHTML = timeDiff;
-            }, 1000)
-        }
-    });
+      gameTimer = setInterval(() => {
+        currentTime = new Date();
+        timeDiff = Math.floor((currentTime - startTime) / 1000);
+        $("#timer")[0].innerHTML = timeDiff;
+      }, 1000);
+    },
+  });
 }
 
 function endGame() {
-    clearInterval(gameTimer);
+  clearInterval(gameTimer);
 
-    $.ajax({
-        method: 'post',
-        url: '/games',
-        data: {
-            user_id: 1, 
-            score: score, 
-            duration: $("#timer")[0].innerHTML, 
-            created_at: new Date(Date.now()).toISOString().replace('T',' ').replace('Z','')
-        }
-    });
+  $.ajax({
+    method: "post",
+    url: "/games",
+    data: {
+      user_id: 1,
+      score: score,
+      duration: $("#timer")[0].innerHTML,
+      created_at: new Date(Date.now())
+        .toISOString()
+        .replace("T", " ")
+        .replace("Z", ""),
+    },
+  });
 
-    location.href = "/";
+  location.href = "/";
 }
 
 function displayPhotoByUrl(url) {
-    $("#campus-photo").attr("src", url);
+  $("#campus-photo").attr("src", url);
 }
 
 function nextSchool() {
-    schoolIndex ++;
-    displayPhotoByUrl(schoolList[schoolIndex].photo_url);
-    $("#location_counter").text(`Location: ${schoolIndex+1}/${schoolList.length}`);
+  schoolIndex++;
+  displayPhotoByUrl(schoolList[schoolIndex].photo_url);
+  $("#location_counter").text(
+    `Location: ${schoolIndex + 1}/${schoolList.length}`
+  );
 }
