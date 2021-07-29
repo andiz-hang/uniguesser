8f2894a1538e290e45ce2c80815b49183d358691
andiz <andiz@sfu.ca>
2021-07-29 12:23:23 -0700

    refactoring

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+// /* ORIGINAL GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.locals.session = req.session
+//   res.render('index', { title: 'Express' });
+// });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.locals.session = req.session
-  res.render('index', { title: 'Express' });
-});
+    res.locals.session = req.session
+    if (!req.session.user_id){
+        res.render('login');
+    } else {
+        res.render('profile');
+    }
+  });
+  
 
 module.exports = router;
