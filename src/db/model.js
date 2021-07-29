e8b25d6eb307f9a00adf39c1981f6d44b4c082b9
jtw14 <jtw14@sfu.ca>
2021-07-28 23:16:36 -0400

    add getHeighscores function

--- src/db/model.js
+++ src/db/model.js
@@ -1,80 +1,93 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 var pool;
 pool = new Pool();
 
-
 async function getCampus() {
-    const query = {
-        text: `SELECT C.campus_name, U.university_name FROM campus C JOIN university U ON C.university_id=U.university_id ORDER BY random()`,
-    }
+  const query = {
+    text: `SELECT C.campus_name, U.university_name FROM campus C JOIN university U ON C.university_id=U.university_id ORDER BY random()`,
+  };
 
-    try {
-        var result = await pool.query(query)
-        return result.rows;
-    } catch (err){
-        console.error(err);
-    }
+  try {
+    var result = await pool.query(query);
+    return result.rows;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function insertScore(userId, data) {
-    const query = {
-        text: `INSERT INTO game_session (user_id, score, duration, created_at) values ($1, $2, $3, $4) RETURNING *`,
-        values: [userId, data.score, (data.duration).toString(), data.created_at]
-    }
+  const query = {
+    text: `INSERT INTO game_session (user_id, score, duration, created_at) values ($1, $2, $3, $4) RETURNING *`,
+    values: [userId, data.score, data.duration.toString(), data.created_at],
+  };
 
-    try {
-        var result = await pool.query(query)
-        return result.rows[0];
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    var result = await pool.query(query);
+    return result.rows[0];
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function registerUser(data) {
-    const query = {
-        text: `INSERT INTO "user" (username, password) VALUES($1, $2) RETURNING *;`,
-        values: [data.username, data.password]
-    }
+  const query = {
+    text: `INSERT INTO "user" (username, password) VALUES($1, $2) RETURNING *;`,
+    values: [data.username, data.password],
+  };
 
-    try {
-        var result = await pool.query(query)
-        return result.rows[0];
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    var result = await pool.query(query);
+    return result.rows[0];
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function loginUser(data) {
-    const query = {
-        text: `SELECT * FROM "user" WHERE username = $1 AND password = $2`,
-        values: [data.username, data.password]
-    }
+  const query = {
+    text: `SELECT * FROM "user" WHERE username = $1 AND password = $2`,
+    values: [data.username, data.password],
+  };
 
-    try {
-        var result = await pool.query(query)
-        return result.rows[0];
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    var result = await pool.query(query);
+    return result.rows[0];
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function getUniversities() {
-    const query = {
-        text: `SELECT * FROM university`
-    }
+  const query = {
+    text: `SELECT * FROM university`,
+  };
 
-    try {
-        var result = await pool.query(query)
-        return result.rows;
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    var result = await pool.query(query);
+    return result.rows;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getHighscores() {
+  const query = {
+    text: `SELECT * FROM game_session ORDER BY score DESC, duration ASC;`,
+  };
+
+  try {
+    var result = await pool.query(query);
+    return result.rows;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = {
-    getCampus,
-    insertScore,
-    registerUser,
-    loginUser,
-    getUniversities
-}
+  getCampus,
+  insertScore,
+  registerUser,
+  loginUser,
+  getUniversities,
+  getHighscores,
+};
