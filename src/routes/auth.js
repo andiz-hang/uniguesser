d943ecbf1e77e6bda9f21f7225459d5ca8fad918
Shanila Javed <sjaved@sfu.ca>
2021-08-01 15:24:16 -0400

    Fix failed merge resolution, sorry

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,33 +12,20 @@ router.get('/login-error', function(req, res, next) {
 
 router.post('/login', async function(req, res, next) {
   if (req.body.username && req.body.password) {
-    if (req.body.action == 'register') {
-      const password_hash = bcrypt.hashSync(req.body.password, BCRYPT_SALT_ROUNDS)
-      const registeredUser = await model.registerUser({
-        username: req.body.username,
-        password: password_hash
-      })
-      req.session.user_id = registeredUser.user_id
-      return res.redirect('/')
-    } else if (req.body.action == 'login') {
-      const user = await model.getUserByUsername(req.body.username)
-      
-      if (!user) {
-        return res.redirect('/login-error')
-      }
-      
-      const password_hash = user.password
-      const isValid = bcrypt.compareSync(req.body.password, password_hash)
+    const user = await model.getUserByUsername(req.body.username)
+    
+    if (!user) {
+      return res.redirect('/login-error')
+    }
+    
+    const password_hash = user.password
+    const isValid = bcrypt.compareSync(req.body.password, password_hash)
 
-      if (isValid) {
-        req.session.user_id = user.user_id;
-        req.session.username = user.username;
-        req.session.email = user.email;
-        req.session.country = user.country;
-        return res.redirect('/')
-      } else {
-        return res.redirect('/login-error')
-      }
+    if (isValid) {
+      setUserSession(req, user)
+      return res.redirect('/')
+    } else {
+      return res.redirect('/login-error')
     }
   }
   
@@ -47,20 +34,29 @@ router.post('/login', async function(req, res, next) {
 
 router.post('/register', async function(req, res, next) {
   if (req.body.username && req.body.password) {
-    var registeredUser = await model.registerUser({
+    const password_hash = bcrypt.hashSync(req.body.password, BCRYPT_SALT_ROUNDS)
+    const registeredUser = await model.registerUser({
       username: req.body.username,
-      password: req.body.password,
+      password: password_hash,
       country: req.body.country
     })
-    req.session.user_id = registeredUser.user_id
+    setUserSession(req, registeredUser)
     return res.redirect('/')
   }
 });
 
 router.post("/logout", async (req, res) => {
   delete req.session.user_id
-
+  
   return res.redirect('/')
 })
 
+function setUserSession(req, user) {
+  req.session.user_id = user.user_id
+  req.session.username = user.username
+  req.session.email = user.email
+  req.session.country = user.country
+}
+
 module.exports = router;
+
